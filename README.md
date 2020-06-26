48db4aa25e9f47a67067bef1505ec820cf44e14b
vosxvo <thonsai@outlook.com>
2020-06-26 15:19:27 +0700

    update repo name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Linux Ultility
+# Linux Utilities
 
 ##### What's news - Updated 25/06/2020
 - Create shortcut function for Ubuntu (Tested on Ubuntu 20.04 LTS)
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+main_menu() {
+    echo "1 - Shortcut Utilities"
+}
+
+shortcut_menu() {
+    clear
+    echo "1 - Create"
+    echo "2 - Delete"
+    echo "3 - Edit"
+}
