9b462793ffc9e82983643d527c0044e1c9959151
vosxvo <thonsai@outlook.com>
2020-09-20 21:26:26 +0700

    Rebuild

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-main_menu() {
-    echo "LINUX UTILITIES"
-    echo "---------------"
-    echo "1 - Desktop Entry Tools"
-    echo ""
+readonly ROOT_UID=0
+readonly UBUNTU_DIR="./script/"
+INSTALLATION_DIR=""
+
+
+detect_os() {
+    local option
+    source /etc/os-release
+    NAME=$NAME
+    VERSION_ID=$VERSION_ID
+    echo "$NAME $VERSION_ID"
 }
+
+detect_os
