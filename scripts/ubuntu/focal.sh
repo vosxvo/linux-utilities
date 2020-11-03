cca1673f74b0362c5919e8cea066649ef031815c
SN <thonsai@outlook.com>
2020-11-03 20:11:28 +0700

    Install LAMP

--- scripts/ubuntu/focal.sh
+++ scripts/ubuntu/focal.sh
@@ -2,16 +2,37 @@
 
 readonly ROOT_UID=0
 
+apache2() {
+    sudo apt install apache2 -y
+    echo "Successful!"
+}
+
+mysql() {
+    sudo apt install mysql-server -y
+    sudo mysql_secure_installation
+    echo "Successful!"
+}
+
+php() {
+    sudo apt install php libapache2-mod-php php-mysql -y
+    echo "Successful!"
+}
+
+lamp() {
+    echo "Installing Apache 2..."
+    apache2
+    echo "Installing MySQL..."
+    mysql
+    echo "Installing PHP..."
+    php
+}
+
 main() {
-    sudo apt update -y
-    sudo apt upgrade -y
-    
+
 }
 
 if [[ $UID -eq $ROOT_UID ]]; then
-    while true; do
-        main
-    done
+    main
 else
     echo "[Warning] This script must be run as root privilege!"
     sudo $0
