1550a47ad07a745e43942c26654c0c96efc42fce
SN <thonsai@outlook.com>
2020-11-05 20:16:27 +0700

    fix bug

--- scripts/firefox-su.sh
+++ scripts/firefox-su.sh
@@ -8,15 +8,21 @@ main() {
     clear
     if [[ -f "$APPLICATIONS_DIR/firefox.desktop" ]]; then
         local option
-        read -p "Do you want backup your Firefox Destop Entry File ? (y|n)" option
+        read -p "Do you want backup your Firefox Destop Entry File ? (y|n) " option
         case $option in
             "y"|"Y") cp "$APPLICATIONS_DIR/firefox.desktop" "$APPLICATIONS_DIR/firefox.desktop.bak";;
-            "n"|"N") ;;
+            "n"|"N") echo "";;
             *) echo "Invalid Option!";;
         esac
         rm "$APPLICATIONS_DIR/firefox.desktop"
     fi
-    cp "../assets/firefox.desktop" "$FIREFOX_DIR/"
+    cp "$(dirname $(pwd))/assets/firefox.desktop" "$APPLICATIONS_DIR/"
+    
+    local option
+    read -p "Successful! Restart now ? (y|n) " option
+    if [[ $option -eq "y" ]] || [[ $option -eq "Y" ]]; then
+        reboot
+    fi
 }
 
 if [[ $UID -eq $ROOT_UID ]]; then
